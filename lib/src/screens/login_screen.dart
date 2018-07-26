e4286454213904db80ad8766c2437b09ac5da63b
Anthony Rozon <arozonm@gmail.com>
2018-07-25 21:20:57 -0400

    Added login form fields

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -10,7 +10,39 @@ class LoginScreenState extends State<LoginScreen> {
   Widget build(context) {
     return Container(
       margin: EdgeInsets.all(20.0),
-      child: Text('Login!'),
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            emailField(),
+            passwordField(),
+            // submitButton(),
+          ],
+        ),
+      ),
     );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com'
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Password'
+      ),
+    );
+  }
+
+  Widget submitButton() {
+
   }
 }
